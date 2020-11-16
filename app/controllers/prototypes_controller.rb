4ab1dafc1865728ebf699797a6949e6e3cd0087a
cacaoman <cacaoman.highend@gmail.com>
2020-11-16 16:25:58 +0900

    さらに修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -59,11 +59,4 @@ class PrototypesController < ApplicationController
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototypes.user
   end
-
-  def move_to_index
-    unless user_signed_in?
-    redirect_to action: :index
-    end
-  end
 end
-
